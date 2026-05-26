92698e9c4e5c085a9bc8b41f406a84e615a91a31
KolamalaSaiDivya <saidivya0607@gmail.com>
2026-05-26 23:23:25 +0530

    Initial setup for shawarma-editor

---
+++ main.c
@@ -0,0 +1,70 @@
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios original_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+void enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &original_termios) == -1) {
+        perror("tcgetattr");
+        exit(1);
+    }
+
+    atexit(disableRawMode);
+
+    struct termios raw = original_termios;
+
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        perror("tcsetattr");
+        exit(1);
+    }
+}
+
+char editorReadKey() {
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) {
+            perror("read");
+            exit(1);
+        }
+    }
+
+    return c;
+}
+
+int main() {
+    enableRawMode();
+
+    while (1) {
+        char c = editorReadKey();
+
+        if (c == 17) {
+            break;
+        }
+
+        printf("You pressed: %d ('%c')\r\n", c, c);
+    }
+
+    return 0;
+}
