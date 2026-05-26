cb114ad818e02b7a54805331083fe0df5ec7d220
KolamalaSaiDivya <saidivya0607@gmail.com>
2026-05-26 23:42:11 +0530

    Cursor Movements

--- main.c
+++ main.c
@@ -5,8 +5,26 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
+
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN
+};
+
+struct editorConfig {
+    int cx;
+    int cy;
+
+    int screenrows;
+    int screencols;
+};
+
+struct editorConfig E;
 
 struct termios original_termios;
 
@@ -25,8 +43,11 @@ void enableRawMode() {
     struct termios raw = original_termios;
 
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+
     raw.c_oflag &= ~(OPOST);
+
     raw.c_cflag |= (CS8);
+
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     raw.c_cc[VMIN] = 0;
@@ -38,7 +59,7 @@ void enableRawMode() {
     }
 }
 
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
 
@@ -49,11 +70,36 @@ char editorReadKey() {
         }
     }
 
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            switch (seq[1]) {
+                case 'A':
+                    return ARROW_UP;
+
+                case 'B':
+                    return ARROW_DOWN;
+
+                case 'C':
+                    return ARROW_RIGHT;
+
+                case 'D':
+                    return ARROW_LEFT;
+            }
+        }
+
+        return '\x1b';
+    }
+
     return c;
 }
 
 void editorDrawRows() {
-    for (int y = 0; y < 24; y++) {
+    for (int y = 0; y < E.screenrows; y++) {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
@@ -64,18 +110,67 @@ void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[H", 3);
 
     editorDrawRows();
+
+    char buf[32];
+
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+
+    write(STDOUT_FILENO, buf, strlen(buf));
+}
+
+void editorMoveCursor(int key) {
+    switch (key) {
+        case ARROW_LEFT:
+            if (E.cx > 0) {
+                E.cx--;
+            }
+            break;
+
+        case ARROW_RIGHT:
+            if (E.cx < E.screencols - 1) {
+                E.cx++;
+            }
+            break;
+
+        case ARROW_UP:
+            if (E.cy > 0) {
+                E.cy--;
+            }
+            break;
+
+        case ARROW_DOWN:
+            if (E.cy < E.screenrows - 1) {
+                E.cy++;
+            }
+            break;
+    }
 }
 
 int main() {
     enableRawMode();
 
+    E.cx = 0;
+    E.cy = 0;
+
+    E.screenrows = 24;
+    E.screencols = 80;
+
     while (1) {
         editorRefreshScreen();
 
-        char c = editorReadKey();
+        int c = editorReadKey();
 
         if (c == 17) {
             break;
+        }
+
+        switch (c) {
+            case ARROW_UP:
+            case ARROW_DOWN:
+            case ARROW_LEFT:
+            case ARROW_RIGHT:
+                editorMoveCursor(c);
+                break;
         }
     }
 
